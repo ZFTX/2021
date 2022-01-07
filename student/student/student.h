8e2023f19854cbcc969245b7d49a70e0e1c84437
ZFTX <95288653+ZFTX@users.noreply.github.com>
2022-01-07 20:12:36 +0800

    Delete student directory

--- student/student/student.h
+++
@@ -1,4 +0,0 @@
-#ifndef	STUDENT_H
-#define	STUDENT_H
-void student(void);
-#endif//STUDENT_H
--- student/student/teacher.h
+++
@@ -1,5 +0,0 @@
-#ifndef TEACHER_H
-#define TEACHER_H
-#include "tools_t.h"
-int teacher(void);
-#endif//TEACHER_H
